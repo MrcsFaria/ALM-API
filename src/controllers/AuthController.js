f6937cf533f0c575e7156c944df9515e3ac84d52
MrcsFaria <contato.marcosvf@gmail.com>
2024-07-27 06:24:37 -0300

    added reset password function

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -122,3 +122,32 @@ export const getCodeByEmail = async (req, res) => {
     res.status(500).json({ message: 'Erro ao buscar código de recuperação.' });
   }
 };
+
+export const ResetPassword = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+      // Verifica se o usuário existe
+      const user = await prisma.user.findUnique({
+          where: { email }
+      });
+
+      if (!user) {
+          return res.status(404).json({ message: 'Usuário não encontrado.' });
+      }
+
+      // Gera o hash da nova senha
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Atualiza a senha do usuário
+      await prisma.user.update({
+          where: { email },
+          data: { password: hashedPassword }
+      });
+
+      res.status(200).json({ message: 'Senha redefinida com sucesso!' });
+  } catch (error) {
+      console.error('Erro ao redefinir a senha:', error);
+      res.status(500).json({ message: 'Erro ao redefinir a senha.' });
+  }
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { signin, signup, handleResetRequest ,getCodeByEmail} from "../controllers/AuthController.js";
+import { signin, signup, handleResetRequest ,getCodeByEmail, ResetPassword} from "../controllers/AuthController.js";
 
 const router = Router();
 
@@ -7,5 +7,6 @@ router.post('/signin', signin);
 router.post('/signup', signup);
 router.post('/send', handleResetRequest);
 router.get('/code/:email', getCodeByEmail);
+router.post('/reset-password', ResetPassword);
 
 export default router;
