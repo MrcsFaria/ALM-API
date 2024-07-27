5f20374902e691e2ec2843cc7d71c3ed75f11757
MrcsFaria <contato.marcosvf@gmail.com>
2024-07-27 05:00:10 -0300

    update send

--- src/server.js
+++ src/server.js
@@ -24,6 +24,33 @@ server.use("/", prodRoutes);
 server.use("/", authRoutes);
 server.use("/", userRoutes);
 
+server.post('/send', async (req, res) => {
+  const { email } = req.body;
+  const code = generateRandomCode();
+
+  const msg = {
+      to: email,
+      from: 'botprojects@outlook.com',
+      subject: 'Recuperação de Senha',
+      text: `Olá, \n\nVocê solicitou a recuperação de senha. Use o código a seguir para recuperar sua senha: ${code}\n\nSe você não solicitou a recuperação, por favor ignore este e-mail.`,
+      html: `<p>Olá,</p>
+             <p>Você solicitou a recuperação de senha. Use o código a seguir para recuperar sua senha:</p>
+             <h2>${code}</h2>
+             <p>Se você não solicitou a recuperação, por favor ignore este e-mail.</p>`,
+  };
+
+  try {
+      await sgMail.send(msg);
+      res.status(200).send('Email enviado com sucesso!');
+  } catch (error) {
+      console.error(error);
+      if (error.response) {
+          console.error(error.response.body);
+      }
+      res.status(500).send('Erro ao enviar e-mail');
+  }
+});
+
 server.listen(process.env.PORT, () => {
   console.log(`Rodando na porta: ${process.env.BASE}`);
 });
